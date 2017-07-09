3ccd2c4214cd9d06a61631b234a3050484fae413
Aqualung <Aqualung@dev-sean.com>
2017-07-09 12:49:08 -0400

    title card

---
+++ Assets/Joe/DropAfterTime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropAfterTime : MonoBehaviour {
+
+    public float hangTime = 1f;
+    private float timeToHang;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        timeToHang = hangTime;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeToHang -= Time.deltaTime;
+        if (timeToHang < 0f) {
+            rb.constraints = RigidbodyConstraints2D.None;
+        }
+        if (timeToHang < hangTime * -5f) {
+            GameObject.Destroy(gameObject);
+        }	
+	}
+}
