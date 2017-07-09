a18ee1952d7208a28a785b164d53876e96f4b9e5
Aqualung <Aqualung@dev-sean.com>
2017-07-09 10:29:00 -0400

    Script for move-once-then-fade-a-bit props

---
+++ Assets/Joe/PropCane.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropCane : MonoBehaviour {
+
+    public bool hasMoved = false;
+    public bool stillNow = false;
+    public float lastMoved;
+    public Collider colly;
+    public Vector3 initialPosition, previousPosition;
+    public float stillTime = 1f;
+
+	// Use this for initialization
+	void Start () {
+        colly = GetComponent<Collider>();
+        initialPosition = previousPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (stillNow) return;
+        if (hasMoved) {
+            // still moving?
+            if (Vector3.Distance(transform.position, previousPosition) > 0.05f) {
+                lastMoved = Time.time;
+                previousPosition = transform.position;
+            } else {
+                if (Time.time > lastMoved + stillTime) {
+                    // immune to further movement by wind
+                    gameObject.tag = "Finish";
+                    if (!FadeAlphaToTarget(0.5f, 0.5f)) stillNow = true;
+                }
+            }
+        } else {
+            if (Vector3.Distance(transform.position, initialPosition) > 0.1f) {
+                hasMoved = true;
+                lastMoved = Time.time;
+            }
+        }
+	}
+
+    // returns true if it can fade, false if already fully faded.
+    private bool FadeAlphaToTarget(float fadeSpeed, float targetAlpha) {
+        Color currentColor = GetComponent<SpriteRenderer>().material.color;
+
+        if (currentColor.a < targetAlpha) {
+            currentColor.a += fadeSpeed * Time.deltaTime;
+            if (currentColor.a > targetAlpha) currentColor.a = targetAlpha;
+        } else if (currentColor.a > targetAlpha) {
+            currentColor.a -= fadeSpeed * Time.deltaTime;
+            if (currentColor.a < targetAlpha) currentColor.a = targetAlpha;
+        } else {
+            return false;
+        }
+        GetComponent<SpriteRenderer>().material.color = currentColor;
+        return true;
+    }
+
+}
