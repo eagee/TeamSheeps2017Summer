e4a0221941367f8434e8dd6b1e898dab1f76aae5
Aqualung <Aqualung@dev-sean.com>
2017-07-09 12:15:44 -0400

    Windmill prefabs

---
+++ Assets/Joe/Windmill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Windmill : MonoBehaviour {
+
+    public float torque = 1f;
+    public float turn = -1f;
+
+    public Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        rb.AddTorque(transform.forward * torque * turn);
+    }
+}
